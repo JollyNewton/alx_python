da740644c84e9314ae41667e5c040a61f2c539bf
JollyNewton <jollycnewton@gmail.com>
2023-08-09 14:55:57 +0300

    Python - Hello, World

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number[-1]>5:
+    print("Last Digit of "number"is "number[-1] "and is less than 5")
+elif number[-1]==0:
+    print("Last Digit of "number"is "number[-1] "and is 0")
+if number[-1]<6 && number[]!=0:
+    print("Last Digit of "number"is "number[-1] "and is less than 6 and not 0")
